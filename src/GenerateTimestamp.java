10af5f858a5beb92285f46138877756ff70ba719
xiangflight <xiangflight@foxmail.com>
2019-07-06 20:04:07 +0800

    add code of onjava8 in chapter 9

---
+++ src/GenerateTimestamp.java
@@ -0,0 +1,11 @@
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2019-07-06 17:47
+ */
+
+public class GenerateTimestamp {
+
+    public static void main(String[] args) {
+        System.out.println(System.currentTimeMillis());
+    }
+}
---
+++ src/chapter9/Transmogrify.java
@@ -0,0 +1,44 @@
+package chapter9;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2019-07-06 16:39
+ */
+class Actor {
+    public void act() {}
+}
+
+class HappyActor extends Actor {
+    @Override
+    public void act() {
+        System.out.println("HappyActor");
+    }
+}
+
+class SadActor extends Actor {
+    @Override
+    public void act() {
+        System.out.println("SadActor");
+    }
+}
+
+class Stage {
+    private Actor actor = new HappyActor();
+
+    public void change() {
+        actor = new SadActor();
+    }
+
+    public void performPlay() {
+        actor.act();
+    }
+}
+
+public class Transmogrify {
+    public static void main(String[] args) {
+        Stage stage = new Stage();
+        stage.performPlay();
+        stage.change();
+        stage.performPlay();
+    }
+}
