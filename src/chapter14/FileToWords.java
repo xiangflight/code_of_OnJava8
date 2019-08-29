41c54cb1e95fd4ce8a3c2c50d1ed99b7e14cbe8c
xiangflight <xiangflight@foxmail.com>
2019-08-29 14:01:14 +0800

    add code of onjava8 in chapter 14

---
+++ src/chapter14/FileToWords.java
@@ -0,0 +1,21 @@
+package chapter14;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2019-08-29 13:57
+ */
+
+public class FileToWords {
+
+    public static Stream<String> stream(String filePath) throws Exception {
+        return Files.lines(Paths.get(filePath))
+                .skip(1)
+                .flatMap(line -> Pattern.compile("\\W+").splitAsStream(line));
+    }
+
+}
