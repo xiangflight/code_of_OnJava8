ab796910098519a1a869128f66d93ff9b90a2dbc
xiangflight <xiangflight@foxmail.com>
2019-10-10 20:36:05 +0800

    end code of onjava8 in chapter 15

---
+++ src/chapter15/InputFile.java
@@ -0,0 +1,54 @@
+package chapter15;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2019/10/10 7:30 下午
+ */
+
+public class InputFile {
+
+    private BufferedReader in;
+
+    public InputFile(String fname) throws Exception {
+        try {
+            in = new BufferedReader(new FileReader(fname));
+        } catch (FileNotFoundException e) {
+            System.out.println("Could not open " + fname);
+            throw e;
+        } catch (Exception e) {
+            try {
+                in.close();
+            } catch (IOException e2) {
+                System.out.println("in.close() unsuccessful");
+                throw e;
+            } finally {
+                // don't close it here
+            }
+        }
+    }
+
+    public String getLine() {
+        String s;
+        try {
+            s = in.readLine();
+        } catch (IOException e) {
+            throw new RuntimeException("readLine() failed");
+        }
+        return s;
+    }
+
+    public void dispose() {
+        try {
+            in.close();
+            System.out.println("dispose() successful");
+        } catch (IOException e2) {
+            throw new RuntimeException("in.close() failed");
+        }
+    }
+
+}
