23ca4ea4b66e9562f6d80e9b0640f79a9396a2d0
xiangflight <xiangflight@foxmail.com>
2019-10-23 22:11:29 +0800

    add code of onjava8 in chapter 16.

---
+++ src/chapter16/SimpleDebugging.java
@@ -0,0 +1,31 @@
+package chapter16;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2019/10/23
+ */
+
+public class SimpleDebugging {
+
+    private static void foo1() {
+        System.out.println("In foo1");
+        foo2();
+    }
+
+    private static void foo2() {
+        System.out.println("In foo2");
+        foo3();
+    }
+
+    private static void foo3() {
+        System.out.println("In foo3");
+        int j = 1;
+        j--;
+        int i = 5 / j;
+    }
+
+    public static void main(String[] args) {
+        foo1();
+    }
+
+}
---
+++ src/chapter16/StringInverter.java
@@ -0,0 +1,12 @@
+package chapter16;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2019/10/21
+ */
+
+public interface StringInverter {
+
+    String invert(String str);
+
+}
