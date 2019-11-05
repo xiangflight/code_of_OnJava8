b1df34da57ac4c5b56b15d32f340667be6f648b0
xiangflight <xiangflight@foxmail.com>
2019-11-05 09:16:07 +0800

    add code of onjava8 in chapter 17.

---
+++ src/chapter17/FileSystemDemo.java
@@ -0,0 +1,36 @@
+package chapter17;
+
+import java.nio.file.FileStore;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2019/11/5
+ */
+
+public class FileSystemDemo {
+
+    static void show(String id, Object o) {
+        System.out.println(id + ": " + o);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("os.name"));
+        FileSystem fsys = FileSystems.getDefault();
+        for (FileStore fs : fsys.getFileStores()) {
+            show("File Store", fs);
+        }
+        for (Path rd: fsys.getRootDirectories()) {
+            show("Root Directory", rd);
+        }
+        show("Separator", fsys.getSeparator());
+        show("UserPrincipalLookupService", fsys.getUserPrincipalLookupService());
+        show("isOpen", fsys.isOpen());
+        show("isReadOnly", fsys.isReadOnly());
+        show("FileSystemProvider", fsys.provider());
+        show("File Attribute Views", fsys.supportedFileAttributeViews());
+    }
+
+}
