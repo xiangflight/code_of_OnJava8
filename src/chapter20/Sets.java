c51cc100ba11fb18198896e944309c73f506785d
xiangflight <xiangflight@foxmail.com>
2019-11-19 12:52:37 +0800

    add code of onjava8 in chapter 20.

---
+++ src/chapter20/Sets.java
@@ -0,0 +1,35 @@
+package chapter20;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2019/11/19
+ */
+
+public class Sets {
+
+    public static <T> Set<T> union(Set<T> a, Set<T> b) {
+        Set<T> result = new HashSet<>(a);
+        result.addAll(b);
+        return result;
+    }
+
+    public static <T> Set<T> intersection(Set<T> a, Set<T> b) {
+        Set<T> result = new HashSet<>(a);
+        result.addAll(b);
+        return result;
+    }
+
+    public static <T> Set<T> difference(Set<T> superset, Set<T> subset) {
+        Set<T> result = new HashSet<>(subset);
+        result.removeAll(subset);
+        return result;
+    }
+
+    public static <T> Set<T> complement(Set<T> a, Set<T> b) {
+        return difference(union(a, b), intersection(a, b));
+    }
+
+}
