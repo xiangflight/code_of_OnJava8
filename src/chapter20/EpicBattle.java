b52381430eae6b1fd639609e5d3f41c1d023ee06
xiangflight <xiangflight@foxmail.com>
2019-11-27 09:25:24 +0800

    add code of onjava8 in chapter 20.

---
+++ src/chapter20/EpicBattle.java
@@ -0,0 +1,95 @@
+package chapter20;
+
+/**
+ * @author xiangdotzhaoAtwoqutechcommacom
+ * @date 2019/11/25
+ */
+interface SuperPower {}
+
+interface XRayVision extends SuperPower {
+    void seeThroughWalls();
+}
+
+interface SuperHearing extends SuperPower {
+    void hearSubtleNoises();
+}
+
+interface SuperSmell extends SuperPower {
+    void trackBySmell();
+}
+
+class SuperHero<POWER extends SuperPower> {
+    POWER power;
+
+    SuperHero(POWER power) {
+        this.power = power;
+    }
+
+    POWER getPower() {
+        return power;
+    }
+}
+
+class SuperSleuth<POWER extends XRayVision> extends SuperHero<POWER> {
+
+    SuperSleuth(POWER power) {
+        super(power);
+    }
+
+    void see() {
+        power.seeThroughWalls();
+    }
+}
+
+class CanineHero<POWER extends SuperHearing & SuperSmell> extends SuperHero<POWER> {
+
+    CanineHero(POWER power) {
+        super(power);
+    }
+
+    void hear() {
+        power.hearSubtleNoises();
+    }
+
+    void smell() {
+        power.trackBySmell();
+    }
+}
+
+class SuperHearSmell implements SuperHearing, SuperSmell {
+
+    @Override
+    public void hearSubtleNoises() {
+
+    }
+
+    @Override
+    public void trackBySmell() {
+
+    }
+}
+
+class DogPerson extends CanineHero<SuperHearSmell> {
+
+    DogPerson() {
+        super(new SuperHearSmell());
+    }
+}
+
+public class EpicBattle {
+
+    static <POWER extends SuperHearing> void useSuperHearing(SuperHero<POWER> hero) {
+        hero.getPower().hearSubtleNoises();
+    }
+
+    static <POWER extends SuperHearing & SuperSmell> void superFind(SuperHero<POWER> hero) {
+        hero.getPower().hearSubtleNoises();
+        hero.getPower().trackBySmell();
+    }
+
+    public static void main(String[] args) {
+        DogPerson dogPerson = new DogPerson();
+        useSuperHearing(dogPerson);
+        superFind(dogPerson);
+    }
+}
